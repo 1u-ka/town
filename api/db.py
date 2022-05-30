3c6267233220f1b65ac309b45f3dcd9bf87782ce
anonymous <anonymous>
2022-05-30 14:24:19 +0200

    to use env var for db conn

--- api/db.py
+++ api/db.py
@@ -1,17 +1,21 @@
+import os
 import psycopg2
 
 class DB:
-  def __init__(self):
-    self.conn = psycopg2.connect(('%s://%s:%s@%s:%d/%s' % ('postgresql',
-                                                        'council',
-                                                        'abcd',
-                                                        'localhost',
-                                                        5434,
-                                                        'urban')))
+  def __init__(self, uri: str = None):
+    if uri is None:
+      uri = ('%s://%s:%s@%s:%d/%s' % ('postgresql',
+                                      'council',
+                                      'abcd',
+                                      'localhost',
+                                      5434,
+                                      'urban'))
+
+    self.conn = psycopg2.connect(uri)
 
   def do(self, stmt: str) -> list:
     curs = self.conn.cursor()
     curs.execute(stmt)
     return curs.fetchall()
 
-db = DB()
+db = DB(os.environ.get('DATABASE_URI'))
